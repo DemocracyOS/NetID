56c80afceb8ae247b16b2d354c56436d845cd84a
slifszyc <sacha.lifszyc@gmail.com>
2013-09-30 17:42:21 -0300

    FOSOAuthServerBundle config

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -17,6 +17,7 @@ class AppKernel extends Kernel
             new Doctrine\Bundle\DoctrineBundle\DoctrineBundle(),
             new Sensio\Bundle\FrameworkExtraBundle\SensioFrameworkExtraBundle(),
             new PdR\NetIdBundle\PdRNetIdBundle(),
+            new FOS\OAuthServerBundle\FOSOAuthServerBundle(),
         );
 
         if (in_array($this->getEnvironment(), array('dev', 'test'))) {
