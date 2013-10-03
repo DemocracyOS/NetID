04e1929220741a37958aa318e8d9a4d9a8b71130
slifszyc <sacha.lifszyc@gmail.com>
2013-10-02 22:57:14 -0300

    Procfile and install.sh files to instal bundle assets

--- install.sh
+++ install.sh
@@ -1,3 +1,7 @@
 php app/console cache:clear --env=prod
 php app/console assets:install --symlink
 php app/console assetic:dump --env=prod
+
+
+php app/console doctrine:schema:update --force
+php app/console doctrine:fixtures:load
