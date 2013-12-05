0e479915c03a00bb1ccb7ed42bcbe34021843c43
slifszyc <sacha.lifszyc@gmail.com>
2013-12-05 12:41:22 -0300

    [export checksum] - Add xls and csv checksum before exporting #21

--- src/DeR/NetIdBundle/Controller/AuditorController.php
+++ src/DeR/NetIdBundle/Controller/AuditorController.php
@@ -19,21 +19,31 @@ class AuditorController extends CRUDController
         // set the theme for the current Admin Form
         $this->get('twig')->getExtension('form')->renderer->setTheme($formView, $this->admin->getFilterTheme());
 
-        $req = $this->getRequest();
-        $req->request->set('format', 'csv');
-        $response = parent::exportAction($this->get('request'));
-        ob_start();
-        $response->sendContent();
-        $content = ob_get_contents();
-        ob_end_clean();
-        $md5Csv = md5($content);
+        $md5Csv = $this->getMd5('csv');
+        $md5Xls = $this->getMd5('xls');
+        $md5s = array();
+        $md5s['csv'] = $md5Csv;
+        $md5s['xls'] = $md5Xls;
 
         return $this->render('DeRNetIdBundle:Default:identity_log_list.html.twig', array(
             'action'     => 'list',
             'form'       => $formView,
             'datagrid'   => $datagrid,
             'csrf_token' => $this->getCsrfToken('sonata.batch'),
-            'md5Csv'     => $md5Csv,
+            'md5s'     => $md5s,
         ));
+    }
+
+    protected function getMd5($format)
+    {
+        $req = $this->getRequest();
+        $req->request->set('format', $format);
+        $response = parent::exportAction($this->get('request'));
+        ob_start();
+        $response->sendContent();
+        $content = ob_get_contents();
+        ob_end_clean();
+        $md5 = md5($content);
+        return $md5;
     }
 }
