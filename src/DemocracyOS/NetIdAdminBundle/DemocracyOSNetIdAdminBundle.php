bf0cb4d81dd9f5657c76ecab9f1256add8dfbca7
slifszyc <sacha.lifszyc@gmail.com>
2013-12-11 13:18:25 -0300

    [initial commit] - Add DemocracyOS/NetIdAdminBundle. Add FOS/UserBundle support.

---
+++ src/DemocracyOS/NetIdAdminBundle/DemocracyOSNetIdAdminBundle.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace DemocracyOS\NetIdAdminBundle;
+
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+
+class DemocracyOSNetIdAdminBundle extends Bundle
+{
+}
---
+++ src/DemocracyOS/NetIdAdminBundle/Entity/User.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace DemocracyOS\NetIdAdminBundle\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+use FOS\UserBundle\Model\User as BaseUser;
+
+/**
+ * User
+ *
+ * @ORM\Entity
+ * @ORM\Table(name="fos_user")
+ */
+class User extends BaseUser
+{
+    /**
+     * @var integer
+     *
+     * @ORM\Column(name="id", type="integer")
+     * @ORM\Id
+     * @ORM\GeneratedValue(strategy="AUTO")
+     */
+    protected $id;
+
+
+    /**
+     * Get id
+     *
+     * @return integer 
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+}
