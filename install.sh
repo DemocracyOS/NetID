2cd0381a6c7ce5ce4c216ee1007c767be1592b7d
slifszyc <sacha.lifszyc@gmail.com>
2013-12-26 11:57:20 -0300

    Few more adjustments for refactor update

--- install.sh
+++
@@ -1,5 +0,0 @@
-php app/console cache:clear --env=prod
-php app/console assets:install --env=prod
-php app/console assetic:dump --env=prod
-php app/console doctrine:schema:update --force
-sh ./bin/run
--- src/DeR/NetIdBundle/DeRNetIdBundle.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-namespace DeR\NetIdBundle;
-
-use Symfony\Component\HttpKernel\Bundle\Bundle;
-
-class DeRNetIdBundle extends Bundle
-{
-}
