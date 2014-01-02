36f7bfc4e592efbc304291fa1ced0128cf0f4c8d
slifszyc <sacha.lifszyc@gmail.com>
2014-01-02 12:50:00 -0300

    Fix on install.sh to run the root user creation command

--- install.sh
+++ install.sh
@@ -2,4 +2,5 @@ php app/console cache:clear --env=prod
 php app/console assets:install --env=prod
 php app/console assetic:dump --env=prod
 php app/console doctrine:schema:update --force
+php app/console netid:root:create
 sh ./bin/run
