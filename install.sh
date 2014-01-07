58e08f1a7bca33a8e3fe63b8ad03021e10804a63
slifszyc <sacha.lifszyc@gmail.com>
2014-01-07 16:04:24 -0300

    [heroku] - Add fixtures loading on heroku deploy

--- install.sh
+++ install.sh
@@ -3,4 +3,5 @@ php app/console assets:install --env=prod
 php app/console assetic:dump --env=prod
 php app/console doctrine:schema:update --force
 php app/console netid:root:create
+php app/console doctrine:fixtures:load
 sh ./bin/run
