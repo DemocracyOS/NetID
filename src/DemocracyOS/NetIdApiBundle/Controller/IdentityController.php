213e7ee59009126253849a3d04eda9bdd78380b4
slifszyc <sacha.lifszyc@gmail.com>
2014-01-13 15:45:50 -0300

    [identity] - Fix old IdentityController namespace

--- src/DemocracyOS/NetIdApiBundle/Controller/IdentityController.php
+++ src/DemocracyOS/NetIdApiBundle/Controller/IdentityController.php
@@ -1,6 +1,6 @@
 <?php
 
-namespace DemocracyOS\NetIdAdminBundle\Controller;
+namespace DemocracyOS\NetIdApiBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Request;
